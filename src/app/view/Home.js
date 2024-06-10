402efa85dd63044eb16d91bc7b24f31aa6b0c55f
vishal vilas kamble <vishalvkamble95@gmail.com>
2024-06-10 11:32:27 +0530

    clg removed

--- src/app/view/Home.js
+++ src/app/view/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import MainLayout from "../../layout/MainLayout";
 
 const Home = () => {
-  console.log("Home Page");
   return (
     <MainLayout>
       <h1>E-Commerce Website</h1>
